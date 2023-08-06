881f5482145d154e883d2de70d41c6616663476d
fadilyas24 <136320719+fadilyas24@users.noreply.github.com>
2023-08-07 06:45:17 +0700

    create script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ Solution of assignment 4:
 
 Expected output:
 
-Hello jammie
-Good Bye eddie
-Good Bye roman
+Hello Yaakov
 Good Bye John
-Hello sue
-Hello phil
-Hello martis
-Hello rashy
-Hello antony
-Good Bye harry
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
 */
 
 (function() {
-    var names = ["jammie", "eddie", "roman", "john", "sue", "phil", "martis", "rashy", "antony", "harry"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
